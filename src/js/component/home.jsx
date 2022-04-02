74717df007858f16662f6d4a7573839133395a83
Lvsyan <Lvsyan96@gmail.com>
2022-04-02 10:38:16 +0000

    Funcionalidad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [select, setSelect] = useState();
+
 	return (
 		<div>
-			
-			
+			<div
+				style={{ height: "3rem", width: "rem" }}
+				className="bg-danger"
+				onClick={() => {
+					setSelect("red");
+				}}></div>
+			<div
+				style={{ height: "3rem", width: "rem" }}
+				className="bg-warning"
+				onClick={() => {
+					setSelect("yellow");
+				}}></div>
+			<div
+				style={{ height: "3rem", width: "rem" }}
+				className="bg-success"
+				onClick={() => {
+					setSelect("green");
+				}}></div>
+				{select == "red" ? <h1>{select}</h1> : <h1>X</h1>}
+
 		</div>
 	);
 };
