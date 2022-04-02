c5c819f38008b2b4741d0e813ecc50050ebdd8d0
Lvsyan <Lvsyan96@gmail.com>
2022-04-02 11:11:29 +0000

    Inicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,33 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+	const [select, setSelect] = useState("red");
 
-	const [select, setSelect] = useState();
-
-	return (
-		<div>
-			<div
-				style={{ height: "3rem", width: "rem" }}
-				className="bg-danger"
-				onClick={() => {
-					setSelect("red");
-				}}></div>
-			<div
-				style={{ height: "3rem", width: "rem" }}
-				className="bg-warning"
-				onClick={() => {
-					setSelect("yellow");
-				}}></div>
-			<div
-				style={{ height: "3rem", width: "rem" }}
-				className="bg-success"
-				onClick={() => {
-					setSelect("green");
-				}}></div>
-				{select == "red" ? <h1>{select}</h1> : <h1>X</h1>}
-
-		</div>
-	);
+	return <div></div>;
 };
 
 export default Home;
