24a7882cc97cea09e4875f4740c543bf1aa8a4aa
Lvsyan <Lvsyan96@gmail.com>
2022-04-03 18:16:59 +0000

    finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,31 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const [select, setSelect] = useState("red");
+	const [selectedColor, setSelectedColor] = useState("red");
 
-	return <div></div>;
+	return (
+		<div className="position-absolute top-50 start-50 translate-middle">
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor == "red" ? " shine" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor == "yellow" ? " shine" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor == "green" ? " shine" : "")
+					}></div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
